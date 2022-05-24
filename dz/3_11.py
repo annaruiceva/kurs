81082fa8fa1f4036f6a9383fe1d66a2a2816fa3c
Anechka <s>
2022-05-24 17:56:00 +0300

    дз 4

--- dz/3_11.py
+++ dz/3_11.py
@@ -1,4 +1,8 @@
-try:
-    print("отрицательное" if (number := float(input("введите число: "))) < 0 else "ноль" if number == 0 else "положительное")
-except:
-    print("введите число!!!")
+def num(number):
+    try:
+        print("отрицательное" if number < 0 else "ноль" if number == 0 else "положительное")
+    except:
+        print("введите число!!!")
+
+
+num(float(input("введите число: ")))
